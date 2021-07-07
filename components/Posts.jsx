c88fc9e3deae3d7e88756cafb6b01f7c08e1432c
Silvia Rodriguez <silviarodriguez@MacBook-Air-de-Silvia.local>
2021-07-07 01:06:31 -0500

    added slider for Posts

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,6 +1,14 @@
 import classNames from 'classNames'
+import Slider from "react-slick";
 
 export default function Posts ({posts}) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
     return (
         <div className='col-span-2 flex-grow-0'>
             <div className={classNames(
@@ -10,23 +18,25 @@ export default function Posts ({posts}) {
                 'text-white',
                 'min-h-full'
                 )}>
-                    <div className='bg- white w-full flex'>
-                        <ul>
+                    <div className='bg- white w-full'>
+                        <Slider {...settings}>
                           {posts.map((post,index)=>(
-                            <li className='flex'>
-                              <img className='mx-auto rounded-full w-10 h-10 'src={post.mentorImage}/>
-                              <div className='p-2'>
-                                <p className='flex items-start lg:text-xl'>{post.postTitle}</p>
-                                <p className=''>{post.postContent}</p>
-                                <i className='block far fa-heart text-3xl text-right'>
-                                  <small className='text-sm'>{post.likes}</small>
-                                </i>
+                            <div>
+                              <div className='flex'>
+                                <img className='mx-auto rounded-full w-10 h-10 'src={post.mentorImage}/>
+                                <div className='p-2'>
+                                  <p className='flex items-start lg:text-xl'>{post.postTitle}</p>
+                                  <p className=''>{post.postContent}</p>
+                                  <i className='block far fa-heart text-3xl text-right'>
+                                    <small className='text-sm'>{post.likes}</small>
+                                  </i>
+                                </div>
                               </div>
-                            </li>
+                            </div>
                           )
                           )}
                             
-                        </ul>
+                        </Slider>
 
                     </div>
                 </div>
