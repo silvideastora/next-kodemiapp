54feb9e0c9dc9a767bdb9f56af5991afbd07ff85
Silvia Rodriguez <silviarodriguez@MacBook-Air-de-Silvia.local>
2021-07-08 20:15:42 -0500

    Added more cards in  ResourcesContainer

--- components/ResourcesContainer.jsx
+++ components/ResourcesContainer.jsx
@@ -1,39 +1,40 @@
 import classNames from 'classnames'
 
-export default function ResourcesContainer() {
+export default function ResourcesContainer({resources}) {
     return (
       <div>
         <ul class={classNames(
           'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'
         )}>
-    <li>
-      <a  className={classNames(
-        'bg-black',
-        'block rounded-lg', 
-        'border-2  border-cyan-ka',
-        'hover:border-cyan-ka hover:text-cyan-ka hover:shadow-lg group',
-        'p-4',
-        'text-white-ka',  
-        )}>
-        <dl className={classNames(
-          'grid sm:block lg:grid grid-cols-2 grid-rows-2',
-          'items-center'
-        )}>
-          <div>
-            <dt>CSS básico y selectores</dt>
-            <dd className={classNames(
-              'font-medium',
-              'group-hover:text-white-ka',
-              'leading-8',
-              'text-gray-inactive'
-            )}>
-              item o recursos
-            </dd>
-          </div>
-        </dl>
-      </a>
-    </li>
-  </ul>
+          {resources.map((resource, index) => (
+          <li>
+            <a  className={classNames(
+              'bg-black',
+              'block rounded-lg', 
+              'border-2  border-cyan-ka',
+              'hover:border-cyan-ka hover:text-cyan-ka hover:shadow-lg group',
+              'p-4',
+              'text-white-ka',  
+              )}>
+              <dl className={classNames(
+                'grid sm:block lg:grid grid-cols-2 grid-rows-2',
+                'items-center'
+              )}>
+                <div>
+                  <dt>CSS básico y selectores</dt>
+                  <dd className={classNames(
+                    'font-medium',
+                    'group-hover:text-white-ka',
+                    'leading-8',
+                    'text-gray-inactive'
+                  )}>
+                    item o recursos
+                  </dd>
+                </div>
+              </dl>
+            </a>
+          </li>))}
+        </ul>
       </div>
 
     )  
--- pages/resources/[topic].js
+++ pages/resources/[topic].js
@@ -1,12 +1,12 @@
 import { useRouter } from 'next/router'
 import ResourcesContainer from '../../components/ResourcesContainer'
+import resources from '../../config/resources.json'
 
-export default function ResourcesScreen (props) {
+export default function ResourcesScreen () {
     const router = useRouter ()
-    console.log(router.query.topic);
     return (
         <div className='flex-wrap p-12 grid grid-cols-auto gap-4'>
-        <ResourcesContainer />    
+        <ResourcesContainer resources={resources}/>    
     </div>
     )
     
