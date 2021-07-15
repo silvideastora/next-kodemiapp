a95dfff364132e06e6987459017330c4fcb107e9
Silvia Rodriguez <silviarodriguez@MacBook-Air-de-Silvia.local>
2021-07-14 23:05:54 -0500

    FIXED identation

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -12,19 +12,19 @@ export default function Home() {
   return (
     <div className='container bg-black'>
       <div className='container h-auto bg-black xs:hidden'>
-      <img className='object-cover h-auto w-full opacity-50' src='../images/banner.jpg'/>
+        <img className='object-cover h-auto w-full opacity-50' src='../images/banner.jpg'/>
       </div>
       <div className={classNames(
         'grid','grid-cols-2','p-2','px-8','sm:py-12','sm:grid-cols-1','md:grid-cols-1', 'md:py-16','lg:grid-cols-3','lg:gap-4', 'xs:grid-cols-1','mx-auto'
         )}>
         <KoderProfileCard koder={koder}/>
         <div className='lg:col-span-2 sm:grid-col-1'>
-        <Posts posts={posts}/>
+          <Posts posts={posts}/>
         </div>
         <div className='col-span-1 lg:col-span-3'>
-        <ModuleBox modules={Modules}/>
+          <ModuleBox modules={Modules}/>
+        </div>
       </div>
-      </div>
-  </div>
+    </div>
   )
 }
