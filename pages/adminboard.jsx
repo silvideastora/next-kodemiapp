2ec859df2e16202a70151c076499bc349720f43a
Víctor Aarón Bárcenas Mondragón <aaronbarcenasm@gmail.com>
2021-07-15 21:09:28 -0500

    ADD layout to adminboard page

--- pages/adminboard.jsx
+++ pages/adminboard.jsx
@@ -5,26 +5,29 @@ import Modules from '../config/modules.json'
 import admin from '../config/admin.json'
 import Posts from '../components/Posts'
 import posts from '../config/posts.json'
+import Layout from '../components/Layout';
 
 
 export default function AdminBoard() {
     return (
-    <div className='container bg-black'>
-    <div className='container h-auto bg-black xs:hidden'>
-    <img className='object-cover h-auto w-full opacity-50' src='../images/banner.jpg'/>
-    </div>
-    <div className={classNames(
-        'grid','grid-cols-2','p-2','px-8','sm:py-12','sm:grid-cols-1','md:grid-cols-1', 'md:py-16','lg:grid-cols-3','lg:gap-4', 'xs:grid-cols-1','mx-auto'
-        )}>
-        <AdminProfileCard admin={admin}/>
-        <div className='lg:col-span-2 sm:grid-col-1'>
-        <Posts posts={posts}/>
+      <Layout footer="false">
+        <div className='container bg-black'>
+          <div className='container h-auto bg-black xs:hidden'>
+            <img className='object-cover h-auto w-full opacity-50' src='../images/banner.jpg'/>
+          </div>
+          <div className={classNames(
+            'grid','grid-cols-2','p-2','px-8','sm:py-12','sm:grid-cols-1','md:grid-cols-1', 'md:py-16','lg:grid-cols-3','lg:gap-4', 'xs:grid-cols-1','mx-auto'
+            )}>
+            <AdminProfileCard admin={admin}/>
+            <div className='lg:col-span-2 sm:grid-col-1'>
+              <Posts posts={posts}/>
+            </div>
+            <div className='col-span-1 lg:col-span-3'>
+              <ModuleBox modules={Modules}/>
+            </div>
+          </div>
         </div>
-        <div className='col-span-1 lg:col-span-3'>
-        <ModuleBox modules={Modules}/>
-    </div>
-    </div>
-</div>
+      </Layout>
 )
 }
 
